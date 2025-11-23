4fe400958eeae6aef6f14dbe82e903e10ba6c6d2
Yashgabani845 <gabaniyash845@gmail.com>
2025-11-23 16:27:58 +0530

    working QR code

--- qrscan/app/page.js
+++ qrscan/app/page.js
@@ -1,6 +1,4 @@
 "use client";
-import { useRouter } from "next/navigation";
-import BarcodeScanner from "./barcode/page";
 import QRScanner from "./scan/page";
 export default function ScanPage() {
   
